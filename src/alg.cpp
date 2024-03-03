4b15f0261141bd3a48095798a18c87b7f21d854e
AndreyChetnev <159932012+AndreyChetnev@users.noreply.github.com>
2024-03-03 14:01:41 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,18 +7,16 @@ double pown(double value, uint16_t n) {
   if (n == 0) {
     return 1;
   }
-  else {
+  else 
     return value * pown(value, n - 1);
-  }
 }
 
 uint64_t fact(uint16_t n) {
   if (n == 1 || n == 0) {
     return 1;
   }
-  else {
+  else 
     return n * fact(n-1);
-  }
 }
 
 double calcItem(double x, uint16_t n) {
@@ -27,7 +25,7 @@ double calcItem(double x, uint16_t n) {
 
 double expn(double x, uint16_t count) {
   double res = 0;
-  for (uint16_t i = 0; i < count; i++) {
+  for (uint16_t i = 0; i <= count; i++) {
     res+= calcItem(x, i);
   }
   return res;
@@ -35,20 +33,20 @@ double expn(double x, uint16_t count) {
 
 double sinn(double x, uint16_t count) {
   double res = 0;
-  uint16_t sign = 1;
+  int sign = 1;
   for (uint16_t i = 1; i < count * 2 + 1; i+=2) {
     res += sign * calcItem(x, i);
-    sign = -sign;
+    sign *= -1;
   }
   return res;
 }
 
 double cosn(double x, uint16_t count) {
   double res = 0;
-  uint16_t sign = 1;
+  int sign = 1;
   for (uint16_t i = 0; i < count * 2; i += 2) {
     res += sign * calcItem(x, i);
-    sign = -sign;
+    sign *= -1;
   }
   return res;
 }
